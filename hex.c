466360dc37e0511f19a27a7fad1e480f800f66ff
abu0295@126.com <abu0295@126.com>
2017-12-24 15:38:56 +0800

    ok

--- hex.c
+++ hex.c
@@ -8,4 +8,8 @@ int main(void){
     printf("0x4d2 = %d  (hex)\n",0x4d2);
     printf("0b10011010010 = %d (binary)\n",0b10011010010);
     printf("02322 = %d (octal,prefix 0 - zero)\n",02322);
+    printf('q');
+    
+    
+    
 }
