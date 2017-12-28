3fb56e6d67a8e1fa6279dcaa0519caf34b525de2
wanggangzipeng <abu0295@126.com>
2017-12-28 21:55:56 +0800

    20171228

---
+++ box.java
@@ -0,0 +1,6 @@
+public class box<T>{
+    private T t;
+    public void set(T t){this.t=t;}
+    public T get() {return t;}
+    
+}
---
+++ tesst.java
@@ -0,0 +1,5 @@
+public class tesst{
+public static void main(String[] args) {
+    box<Integer> a = new box();
+    a.set(9);
+}}
